905471a1ece5d6708aef2a33ed555d0c0cc7929c
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2025-04-21 16:39:05 +0200

    Add initial App and main entry files for the Browser Extension Manager

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Header from './components/Header';
+import ExtensionList from './components/ExtensionList';
+
+function App() {
+  return (
+    <main className='container'>
+      <Header />
+      <ExtensionList />
+    </main>
+  );
+}
+export default App;
